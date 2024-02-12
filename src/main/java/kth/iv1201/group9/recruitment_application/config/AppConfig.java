1774a9043270e2de44bf9a150d160e72526d362c
Jesper Victorsson <jesper.victorsson@gmail.com>
2024-02-12 10:43:00 +0100

    1.1 updated
    works like it should right now, will only be added to our proper pages

--- src/main/java/kth/iv1201/group9/recruitment_application/config/AppConfig.java
+++ src/main/java/kth/iv1201/group9/recruitment_application/config/AppConfig.java
@@ -10,6 +10,9 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.i18n.AcceptHeaderLocaleResolver;
 import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
 import org.springframework.web.servlet.i18n.SessionLocaleResolver;
+import org.thymeleaf.spring6.SpringTemplateEngine;
+import org.thymeleaf.spring6.templateresolver.SpringResourceTemplateResolver;
+import org.thymeleaf.spring6.view.ThymeleafViewResolver;
 
 @Configuration
 public class AppConfig implements WebMvcConfigurer {
@@ -41,5 +44,33 @@ public class AppConfig implements WebMvcConfigurer {
         messageSource.setDefaultEncoding("UTF-8");
         return messageSource;
 }
+
+@Bean
+public SpringTemplateEngine templateEngine() {
+    SpringTemplateEngine templateEngine = new SpringTemplateEngine();
+    templateEngine.setTemplateResolver(templateResolver());
+    templateEngine.setMessageSource(messageSource());
+    return templateEngine;
+}
+
+@Bean
+public ThymeleafViewResolver thymeleafViewResolver() {
+    ThymeleafViewResolver resolver = new ThymeleafViewResolver();
+    resolver.setTemplateEngine(templateEngine());
+    resolver.setCharacterEncoding("UTF-8");
+    return resolver;
+}
+
+    @Bean
+    public SpringResourceTemplateResolver templateResolver() {
+        SpringResourceTemplateResolver templateResolver = new SpringResourceTemplateResolver();
+        templateResolver.setPrefix("classpath:/templates/");
+        templateResolver.setSuffix(".html");
+        templateResolver.setTemplateMode("HTML");
+        templateResolver.setCharacterEncoding("UTF-8");
+        templateResolver.setCacheable(false); // Disable caching for development
+        return templateResolver;
+    }
+
 
 }
